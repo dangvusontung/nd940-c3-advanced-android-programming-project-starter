6ebd2e1853cf116efe627435dc63c89d345a0fc4
TungDVS <dangvusontung@gmail.com>
2022-12-13 23:31:21 +0700

    Refactor code

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -9,6 +9,7 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
@@ -16,27 +17,30 @@ import kotlin.properties.Delegates
 private const val CIRCLE_ANIMATION = "CIRCLE_ANIMATION"
 private const val ANIMATION_FILL = "ANIMATION_FILL"
 
+private const val TAG = "LoadingButton"
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+//    View & animation attribute
     private var widthSize = 0
     private var heightSize = 0
     private var textSize = 0F
     private var defaultBackgroundColor = 0
     private var loadingBackgroundColor = 0
     private var textColor = 0
-    private var standardTitle = ""
+    private var defaultTitle = ""
     private var loadingTitle = ""
     private var loadingColor = 0
-
     private var isLoading = false
-    private var fulfillWidth = 0F
-    private var sweepAngle = 0F
-
+    private var loadingWidth = 0F
+    private var loadingCircleAngle = 0F
     private val cornerRadius = resources.getDimension(R.dimen.corner_radius)
 
+//     Animator
     private val valueAnimator = ValueAnimator()
 
+//    Paint object
     private val paint = Paint().apply {
         isAntiAlias = true
         strokeWidth = resources.getDimension(R.dimen.stroke_width)
@@ -47,23 +51,21 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
-
     init {
         context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
             defaultBackgroundColor = getColor(R.styleable.LoadingButton_standardBackgroundColor, 0)
             loadingBackgroundColor = getColor(R.styleable.LoadingButton_activeBackgroundColor, 0)
             textColor = getColor(R.styleable.LoadingButton_textColor, 0)
             loadingColor = getColor(R.styleable.LoadingButton_loadingColor, 0)
-            standardTitle = getString(R.styleable.LoadingButton_standardTitle) ?: ""
+            defaultTitle = getString(R.styleable.LoadingButton_standardTitle) ?: ""
             loadingTitle = getString(R.styleable.LoadingButton_activeTitle) ?: ""
             textSize = getDimension(R.styleable.LoadingButton_textSize, 0F)
         }
     }
 
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        drawClippedRectangle(canvas)
+        drawAnimation(canvas)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -86,24 +88,22 @@ class LoadingButton @JvmOverloads constructor(
         return true
     }
 
-    private fun drawClippedRectangle(canvas: Canvas) {
+    private fun drawAnimation(canvas: Canvas) {
+        Log.d(TAG, "drawAnimation: ")
         canvas.save()
         canvas.clipRect(
             0F, 0F,
             widthSize.toFloat(), heightSize.toFloat()
         )
         canvas.drawColor(defaultBackgroundColor)
-
         paint.color = loadingBackgroundColor
-        canvas.drawRect(0F, 0F, fulfillWidth, heightSize.toFloat(), paint)
-
-
+        canvas.drawRect(0F, 0F, loadingWidth, heightSize.toFloat(), paint)
         paint.apply {
             color = textColor
             textAlign = Paint.Align.CENTER
         }
 
-        val title = if (isLoading) loadingTitle else standardTitle
+        val title = if (isLoading) loadingTitle else defaultTitle
         val rectBoundText = Rect()
         paint.getTextBounds(title, 0, title.length, rectBoundText)
         val xTitle = widthSize.toFloat() / 2
@@ -115,22 +115,23 @@ class LoadingButton @JvmOverloads constructor(
             val left = xTitle + rectBoundText.width() / 2
             val top = heightSize.toFloat() / 2 - cornerRadius / 4
             val rectBoundCircle = RectF(left, top, left + cornerRadius, top + cornerRadius)
-            canvas.drawArc(rectBoundCircle, 0F, sweepAngle, true, paint)
+            canvas.drawArc(rectBoundCircle, 0F, loadingCircleAngle, true, paint)
         }
 
         canvas.restore()
     }
 
     private fun startAnimation() {
-        val sweepPropertyValuesHolder = PropertyValuesHolder.ofFloat(CIRCLE_ANIMATION, 0F, 360F)
-        val fillWidthPropertyValuesHolder = PropertyValuesHolder.ofFloat(ANIMATION_FILL, 0f, widthSize.toFloat())
+        val circleAnimationPropertyValuesHolder = PropertyValuesHolder.ofFloat(CIRCLE_ANIMATION, 0F, 360F)
+        val fillAnimationPropertyValuesHolder =
+            PropertyValuesHolder.ofFloat(ANIMATION_FILL, 0f, widthSize.toFloat())
 
-        valueAnimator.setValues(sweepPropertyValuesHolder, fillWidthPropertyValuesHolder)
-        valueAnimator.duration = 3000
+        valueAnimator.setValues(circleAnimationPropertyValuesHolder, fillAnimationPropertyValuesHolder)
+        valueAnimator.duration = 5000
 
         valueAnimator.addUpdateListener { animator ->
-            sweepAngle = animator.getAnimatedValue(CIRCLE_ANIMATION) as Float
-            fulfillWidth = animator.getAnimatedValue(ANIMATION_FILL) as Float
+            loadingCircleAngle = animator.getAnimatedValue(CIRCLE_ANIMATION) as Float
+            loadingWidth = animator.getAnimatedValue(ANIMATION_FILL) as Float
             invalidate()
         }
 
@@ -157,5 +158,4 @@ class LoadingButton @JvmOverloads constructor(
 
         valueAnimator.start()
     }
-
 }
